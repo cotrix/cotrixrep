54aefb6c801c906bc21efc6ae0dd8722a12dc424
fabio.simeoni <fabio.simeoni@fao.org>
2013-02-22 22:19:15 +0100

    Revert "merged"
    This reverts commit 61d8cd2ce50de82cf532c7b9c5f027c415f1bd8c.

--- cotrix/cotrix-web-importwizard/src/main/java/org/cotrix/web/importwizard/client/presenter/Presenter.java
+++
@@ -1,6 +0,0 @@
-package org.cotrix.web.importwizard.client.presenter;
-import com.google.gwt.user.client.ui.HasWidgets;
-
-public abstract interface Presenter<T> {
-  public abstract void go(final HasWidgets container);
-}
