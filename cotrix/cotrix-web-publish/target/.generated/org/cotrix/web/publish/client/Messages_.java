f48efd4d535237a221af6de29bdff2bb1913d2a6
Sukrit Chotepatarasri <sukrit@touchsi.com>
2013-03-26 22:09:22 +0700

    merge

---
+++ cotrix/cotrix-web-publish/target/.generated/org/cotrix/web/publish/client/Messages_.java
@@ -0,0 +1,12 @@
+package org.cotrix.web.publish.client;
+
+public class Messages_ implements org.cotrix.web.publish.client.Messages {
+  
+  public java.lang.String nameField() {
+    return "Enter your name";
+  }
+  
+  public java.lang.String sendButton() {
+    return "Send";
+  }
+}
