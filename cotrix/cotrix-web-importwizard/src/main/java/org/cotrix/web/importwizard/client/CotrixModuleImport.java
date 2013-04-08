5d973af52de495c451a294af513e70a0b8470254
Sukrit Chotepatarasri <sukrit@touchsi.com>
2013-04-08 14:18:44 +0700

    disable onmodule load

--- cotrix/cotrix-web-importwizard/src/main/java/org/cotrix/web/importwizard/client/CotrixModuleImport.java
+++ cotrix/cotrix-web-importwizard/src/main/java/org/cotrix/web/importwizard/client/CotrixModuleImport.java
@@ -45,7 +45,7 @@ public class CotrixModuleImport implements EntryPoint {
 				Window.alert(caught.getMessage());
 			}
 		});*/
-	/*	CotrixImportAppGinInjector injector = GWT.create(CotrixImportAppGinInjector.class);
+		/*CotrixImportAppGinInjector injector = GWT.create(CotrixImportAppGinInjector.class);
 		CotrixImportAppController appViewer = injector.getAppController();
 		appViewer.go(RootPanel.get());*/
 	}
