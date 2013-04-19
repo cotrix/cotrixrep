0df47a1b01ca9ca5b20b68f308e88e55073571e2
fabiosimeoni <fabio.simeoni@me.com>
2013-04-19 09:29:33 +0200

    fixed previous commit

---
+++ cotrix/cotrix-repository/src/main/java/org/cotrix/repository/query/CodebagQuery.java
@@ -0,0 +1,7 @@
+package org.cotrix.repository.query;
+
+import org.cotrix.domain.Codebag;
+
+public interface CodebagQuery<R> extends Query<Codebag,R> {
+
+}
---
+++ cotrix/cotrix-repository/src/main/java/org/cotrix/repository/query/CodelistQuery.java
@@ -0,0 +1,7 @@
+package org.cotrix.repository.query;
+
+import org.cotrix.domain.Codelist;
+
+public interface CodelistQuery<R> extends Query<Codelist,R> {
+
+}
