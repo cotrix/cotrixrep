4511c32a54aa4c08d6a4b06c53d39179f5c9c16a
Sukrit Chotepatarasri <sukrit@touchsi.com>
2013-05-03 19:19:32 +0700

    add HeaderType Validation (Only one code is allowed)

--- cotrix/cotrix-web-importwizard/src/main/java/org/cotrix/web/importwizard/client/view/form/HeaderTypeListBox.java
+++ cotrix/cotrix-web-importwizard/src/main/java/org/cotrix/web/importwizard/client/view/form/HeaderTypeListBox.java
@@ -2,9 +2,11 @@ package org.cotrix.web.importwizard.client.view.form;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
@@ -29,7 +31,12 @@ public class HeaderTypeListBox extends Composite {
 	}
 	
 	@UiField ListBox listbox;
-	
+	@UiField Style style;
+	interface Style extends CssResource {
+		String listBoxError();
+		String listBox();
+	}
+
 	@UiHandler("listbox")
 	void onChangeHandler(ChangeEvent event) {
 		onHeaderTypeSelectedHandler.onHeaderTypeSelected(listbox.getSelectedIndex());
@@ -37,6 +44,15 @@ public class HeaderTypeListBox extends Composite {
 	
 	public void setOnHeaderTypeSelectedHandler(OnHeaderTypeSelectedHandler onHeaderTypeSelectedHandler){
 		this.onHeaderTypeSelectedHandler = onHeaderTypeSelectedHandler;
+	}
+	
+	public void setStyleError(){
+		Window.alert("set list box style");
+		this.listbox.setStyleName(style.listBoxError());
+	}
+	
+	public void setStyleNormal(){
+		this.listbox.setStyleName(style.listBox());
 	}
 	
 	public String getValue(){
