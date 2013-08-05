ece34ee27d6c9573eea249975a454202079a1028
Federico De Faveri <federico.defaveri@fao.org>
2013-08-05 12:18:05 +0200

    Updated selection step: new title/tracker label. Swapped buttons

--- cotrix/cotrix-web-importwizard/src/main/java/org/cotrix/web/importwizard/client/step/sourceselection/SourceSelectionStepPresenterImpl.java
+++ cotrix/cotrix-web-importwizard/src/main/java/org/cotrix/web/importwizard/client/step/sourceselection/SourceSelectionStepPresenterImpl.java
@@ -26,7 +26,7 @@ public class SourceSelectionStepPresenterImpl extends AbstractWizardStep impleme
 
 	@Inject
 	public SourceSelectionStepPresenterImpl(SourceSelectionStepView view) {
-		super("sourceSelection", "Source Selection", "Select source", NavigationButtonConfiguration.DEFAULT_BACKWARD, NavigationButtonConfiguration.NONE);
+		super("sourceSelection", "Locate", "Where is the codelist?", NavigationButtonConfiguration.DEFAULT_BACKWARD, NavigationButtonConfiguration.NONE);
 		this.view = view;
 		this.view.setPresenter(this);
 	}
