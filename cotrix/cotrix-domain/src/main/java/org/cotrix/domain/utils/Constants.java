3ec3a77945d5e7a125c2a841a6cab668872622fc
fabiosimeoni <fabio.simeoni@me.com>
2013-09-19 18:01:37 +0200

    few more predefined attribute types

--- cotrix/cotrix-domain/src/main/java/org/cotrix/domain/utils/Constants.java
+++ cotrix/cotrix-domain/src/main/java/org/cotrix/domain/utils/Constants.java
@@ -5,7 +5,12 @@ import javax.xml.namespace.QName;
 public class Constants {
 
 	public static final String NS = "http://cotrix.org";
+
+	public static final QName DESCRIPTION_TYPE = new QName(NS,"description");
+	public static final QName ANNOTATION_TYPE = new QName(NS,"annotation");
+	public static final QName NAME_TYPE = new QName(NS,"name");
 	
-	public static final QName DEFAULT_TYPE = new QName(NS,"description");
+	public static final QName DEFAULT_TYPE = DESCRIPTION_TYPE;
+	
 	public static final QName NAME = new QName(NS,"name");
 }
