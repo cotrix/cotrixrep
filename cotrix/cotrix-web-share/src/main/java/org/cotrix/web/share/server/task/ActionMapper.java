8eae38a92d0846a7dc5ea38df06b422e4adb0c63
Federico De Faveri <federico.defaveri@fao.org>
2013-10-01 14:33:19 +0200

    Removed wrong import

--- cotrix/cotrix-web-share/src/main/java/org/cotrix/web/share/server/task/ActionMapper.java
+++ cotrix/cotrix-web-share/src/main/java/org/cotrix/web/share/server/task/ActionMapper.java
@@ -3,11 +3,13 @@
  */
 package org.cotrix.web.share.server.task;
 
+import java.util.AbstractMap;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import javax.enterprise.inject.Default;
@@ -15,8 +17,8 @@ import javax.inject.Singleton;
 
 import org.cotrix.action.Action;
 import org.cotrix.web.share.shared.feature.UIFeature;
-
-import com.google.gwt.dev.util.collect.HashSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author "Federico De Faveri federico.defaveri@fao.org"
@@ -26,12 +28,24 @@ import com.google.gwt.dev.util.collect.HashSet;
 @Singleton
 public class ActionMapper {
 	
+	protected Logger logger = LoggerFactory.getLogger(ActionMapper.class);
+	
 	protected Map<Action, Set<UIFeature>> mapping = new HashMap<Action, Set<UIFeature>>();
 	
 	public Set<UIFeature> mapActions(Collection<Action> actions)
 	{
-		
-		return Collections.emptySet();
+		Set<UIFeature> features = new HashSet<UIFeature>();
+		for (Action action:actions) {
+			Action generic = action.onAny();
+			Set<UIFeature> actionFeatures = mapping.get(generic);
+			if (actionFeatures!=null) features.addAll(actionFeatures);
+			else {
+				logger.warn("Action {} as generic {} has no UIFeatures", action, generic);
+				logger.warn("Current registered actions:");
+				for (Entry<Action, Set<UIFeature>> entry:mapping.entrySet()) logger.warn("{} -> {}", entry.getKey(), entry.getValue());
+			}
+		}
+		return features;
 	}
 	
 	public ActionMapBuilder map(Action action)
@@ -43,5 +57,4 @@ public class ActionMapper {
 	{
 		mapping.put(action, new HashSet<UIFeature>(Arrays.asList(features)));
 	}
-
 }
