21b8f8fdd7c12b1a80d5f07df374e281216c1215
Federico De Faveri <federico.defaveri@fao.org>
2013-10-22 15:51:34 +0200

    Added workaround filter to WELD issue in jetty

---
+++ cotrix/cotrix-web/src/main/java/org/cotrix/web/portlet/WeldWorkaroundFilter.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package org.cotrix.web.portlet;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.jboss.weld.servlet.SessionHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author "Federico De Faveri federico.defaveri@fao.org"
+ *
+ */
+public class WeldWorkaroundFilter implements Filter {
+	
+	protected Logger logger = LoggerFactory.getLogger(WeldWorkaroundFilter.class);
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,	FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+		HttpSession session = httpServletRequest.getSession();
+		SessionHolder.sessionCreated(session);
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		logger.trace("initializing WeldWorkaroundFilter");
+	}
+
+}
