fff67b4d0726d7180de45177ce36bccb976217c8
Federico De Faveri <federico.defaveri@fao.org>
2013-10-29 16:53:30 +0100

    Warning cleaning

--- cotrix/cotrix-web-importwizard/src/main/java/org/cotrix/web/importwizard/client/CotrixImportAppGinInjector.java
+++ cotrix/cotrix-web-importwizard/src/main/java/org/cotrix/web/importwizard/client/CotrixImportAppGinInjector.java
@@ -1,4 +1,5 @@
 package org.cotrix.web.importwizard.client;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.inject.client.GinModules;
 import com.google.gwt.inject.client.Ginjector;
 
@@ -9,6 +10,8 @@ import org.cotrix.web.share.client.CommonGinModule;
 
 @GinModules({CotrixImportAppGinModule.class, CommonGinModule.class})
 public interface CotrixImportAppGinInjector extends Ginjector {
+	
+	public static CotrixImportAppGinInjector INSTANCE = GWT.create(CotrixImportAppGinInjector.class);
 	
     public ImportServiceAsync getRpcService();
     
