7aca0e5fb0a608a4b07517c1a76d33d22ce61ce3
fabiosimeoni <fabio.simeoni@me.com>
2013-11-15 04:43:17 +0100

    sdmx mapping directives now based on (name,type) pairs

--- cotrix/cotrix-io/src/main/java/org/cotrix/io/sdmx/map/Codelist2SdmxDirectives.java
+++ cotrix/cotrix-io/src/main/java/org/cotrix/io/sdmx/map/Codelist2SdmxDirectives.java
@@ -1,5 +1,7 @@
 package org.cotrix.io.sdmx.map;
 
+import static org.cotrix.common.Utils.*;
+import static org.cotrix.domain.dsl.Codes.*;
 import static org.cotrix.io.sdmx.Constants.*;
 
 import java.util.HashMap;
@@ -7,30 +9,40 @@ import java.util.Map;
 
 import javax.xml.namespace.QName;
 
+import org.cotrix.domain.Attribute;
 import org.cotrix.io.MapService.MapDirectives;
 import org.cotrix.io.sdmx.SdmxElement;
 import org.sdmxsource.sdmx.api.model.beans.codelist.CodelistBean;
 
 public class Codelist2SdmxDirectives implements MapDirectives<CodelistBean> {
 
-	public static final Codelist2SdmxDirectives DEFAULT = new Codelist2SdmxDirectives();
-
+	public static interface MappingClause {
+		
+		/**
+		 * Maps the attribute name and type onto an SDMX element type
+		 * @param to the element type
+		 * @return this directives
+		 */
+		Codelist2SdmxDirectives to(SdmxElement e);
+	}
 	
-	private Map<QName,SdmxElement> elements = new HashMap<QName,SdmxElement>();
+	public static Codelist2SdmxDirectives DEFAULT = new Codelist2SdmxDirectives();
+	
+	private static Map<QName,SdmxElement> defaults = new HashMap<QName,SdmxElement>();
+	
+	static {
+		
+		for (SdmxElement e : SdmxElement.values())
+			defaults.put(e.defaultType(),e);
+	}
+
+	private final Map<Attribute,SdmxElement> attributeDirectives = new HashMap<Attribute,SdmxElement>();
 	
 	private String agency = DEFAULT_SDMX_AGENCY;
 	private String name;
 	private String version;
 	private Boolean isFinal;
 	
-	/**
-	 * Creates an instance with default directives.
-	 */
-	public Codelist2SdmxDirectives() {
-
-		for (SdmxElement e : SdmxElement.values())
-			map(e.defaultType(),e);
-	}
 	
 	public void name(String name) {
 		this.name = name;
@@ -64,12 +76,68 @@ public class Codelist2SdmxDirectives implements MapDirectives<CodelistBean> {
 		return version;
 	}
 	
-	public void map(QName name,SdmxElement element) {
+	
+	public SdmxElement get(Attribute attribute) {
 		
-		elements.put(name,element);
+		SdmxElement element = attributeDirectives.get(canonicalFormOf(attribute));
+		
+		if (element==null && attribute.type()!=null)
+			element = defaults.get(attribute.type());
+		
+		return element;
 	}
 	
-	public SdmxElement get(QName name) {
-		return elements.get(name);
+	
+	/**
+	 * Maps an attribute name and type onto a SDMX element type
+	 * @param name the name
+	 * @param type the type
+	 * @return the mapping clause
+	 */
+	public MappingClause map(final QName name, final QName type) {
+		
+		notNull("name",name);
+		notNull("type",type);
+		
+		return new MappingClause() {
+			
+			@Override
+			public Codelist2SdmxDirectives to(SdmxElement element) {
+				notNull("SDMX element",element);
+				
+				attributeDirectives.put(templateFrom(name, type),element);
+				
+				return Codelist2SdmxDirectives.this;
+			}
+		};
+		
+		
 	}
+	
+	/**
+	 * Maps an attribute name and type onto a SDMX element type
+	 * @param name the name
+	 * @param type the type
+	 * @return the mapping clause
+	 */
+	public MappingClause map(String name, String type) {
+		
+		notNull("name",name);
+		notNull("type",type);
+		
+		return map(q(name),q(type));
+		
+	}
+	
+	
+	//helpers
+	
+	public Attribute canonicalFormOf(Attribute a) {
+		return templateFrom(a.name(),a.type());
+	}
+	
+	public Attribute templateFrom(QName name, QName type) {
+		return attr().name(name).value("?").ofType(type).build();
+	}
+	
 }
