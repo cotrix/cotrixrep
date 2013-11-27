09be40a740b31e6bf95d370ca9fc75b42a50998b
fabiosimeoni <fabio.simeoni@me.com>
2013-11-27 13:12:22 +0100

    role-based deleg.policies, role propagation respects any

--- cotrix/cotrix-application/src/main/java/org/cotrix/application/DelegationPolicy.java
+++ cotrix/cotrix-application/src/main/java/org/cotrix/application/DelegationPolicy.java
@@ -1,6 +1,7 @@
 package org.cotrix.application;
 
 import org.cotrix.action.Action;
+import org.cotrix.domain.user.Role;
 import org.cotrix.domain.user.User;
 
 public interface DelegationPolicy {
@@ -8,4 +9,8 @@ public interface DelegationPolicy {
 	void validateDelegation(User source, User target, Action ... actions);
 	
 	void validateRevocation(User source, User target, Action ... actions);
+	
+	void validateDelegation(User source, User target, Role ... roles);
+	
+	void validateRevocation(User source, User target, Role ... roles);
 }
--- cotrix/cotrix-application/src/test/java/org/acme/DefaultDelegationPolicyTest.java
+++ cotrix/cotrix-application/src/test/java/org/acme/DefaultDelegationPolicyTest.java
@@ -1,11 +1,13 @@
 package org.acme;
 
 import static org.cotrix.action.Actions.*;
+import static org.cotrix.action.ResourceType.*;
 import static org.cotrix.domain.dsl.Users.*;
 
 import org.cotrix.action.Action;
 import org.cotrix.application.DelegationPolicy;
 import org.cotrix.application.impl.DefaultDelegationPolicy;
+import org.cotrix.domain.user.Role;
 import org.cotrix.domain.user.User;
 import org.junit.Test;
 
@@ -37,7 +39,6 @@ public class DefaultDelegationPolicyTest {
 		policy.validateDelegation(joe, bill, doit);
 	}
 	
-	
 	@Test
 	public void delegatesTemplatesForRootUsers() {
 		
@@ -47,5 +48,22 @@ public class DefaultDelegationPolicyTest {
 		policy.validateDelegation(joe, bill, doit);
 
 	}
+	
+	@Test
+	public void delegatesRolesWithTemplatesOfDifferentTypesForNonRootUsers() {
+		
+		Action doGeneric = action(application,"generic");
+		Role something = user().name("r1").fullName("r1").can(doGeneric).buildAsRoleFor(application);
+		
+		Action doSpecific = action(codelists,"specific");
+		Role somethingElse = user().name("r2").fullName("r2").is(something).can(doSpecific).buildAsRoleFor(codelists);
+		
+		User joe = user().name("joe").fullName("joe").is(somethingElse.on("1")).build();
+		User bill = user().name("bill").fullName("bill").build();
+		
+		policy.validateDelegation(joe, bill, somethingElse.on("1"));
+		
+	}
+
 		
 }
