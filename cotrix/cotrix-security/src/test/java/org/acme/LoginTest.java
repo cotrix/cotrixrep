e87247e9f620aa7d0e9f28f1273f23856e4d315d
fabiosimeoni <fabio.simeoni@me.com>
2013-12-19 00:36:44 +0100

    moved to Java 7 and refactored test facilities

--- cotrix/cotrix-security/src/test/java/org/acme/LoginTest.java
+++ cotrix/cotrix-security/src/test/java/org/acme/LoginTest.java
@@ -4,77 +4,55 @@ import static org.cotrix.action.UserAction.*;
 import static org.cotrix.domain.dsl.Users.*;
 import static org.cotrix.security.impl.DefaultNameAndPasswordCollector.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
-import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.SessionScoped;
-import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
-import org.cotrix.common.cdi.BeanSession;
 import org.cotrix.common.cdi.Current;
-import org.cotrix.domain.dsl.Users;
 import org.cotrix.domain.user.User;
-import org.cotrix.memory.repository.MUserRepository;
-import org.cotrix.repository.UserQueries;
-import org.cotrix.repository.impl.DefaultUserRepository;
-import org.cotrix.repository.utils.UuidGenerator;
-import org.cotrix.security.impl.DefaultLoginService;
-import org.cotrix.security.impl.DefaultNameAndPasswordCollector;
-import org.cotrix.security.impl.DefaultSignupService;
-import org.cotrix.security.impl.MRealm;
+import org.cotrix.security.LoginService;
+import org.cotrix.security.SignupService;
 import org.cotrix.test.ApplicationTest;
-import org.jglue.cdiunit.AdditionalClasses;
-import org.jglue.cdiunit.CdiRunner;
-import org.jglue.cdiunit.ContextController;
-import org.jglue.cdiunit.DummyHttpRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-@RunWith(CdiRunner.class)
-@AdditionalClasses({Users.class,DefaultUserRepository.class, UserQueries.QueryFactoryInjector.class, MUserRepository.class, DefaultNameAndPasswordCollector.class, MRealm.class, UuidGenerator.class })
+import com.googlecode.jeeunit.JeeunitRunner;
+
+@RunWith(JeeunitRunner.class)
 public class LoginTest extends ApplicationTest {
 
-	@Inject
-	ContextController contextController;
-	
 	@Inject
 	@Current
 	User currentUser;
 
 	@Inject
-	DefaultLoginService service;
+	LoginService service;
 	
 	@Inject
-	DefaultSignupService signupService;
-	
+	SignupService signupService;
+
+	HttpServletRequest req = mock(HttpServletRequest.class);
+
 	@Test
 	public void loginAsGuestIfNoTokenIsAvailable() throws Exception {
 	
-		DummyHttpRequest req = new DummyHttpRequest();
-		
-		contextController.openRequest(req);
-		
 		User logged = service.login(req);
 
 		assertEquals(guest, logged);
 
-		// annoyingly we cannot compare objects as current currentUser is a proxy
-		assertEquals(guest.toString(), currentUser.toString());
+		//we cannot compare objects as current currentUser is a CDI proxy
+		assertEquals(guest.id(), currentUser.id());
 		
-		contextController.closeRequest();
 	}
 
 	@Test
 	public void loginAsCotrix() throws Exception {
 		
-		DummyHttpRequest r = new DummyHttpRequest();
+		when(req.getAttribute(nameParam)).thenReturn(cotrix.name());
+		when(req.getAttribute(pwdParam)).thenReturn(cotrix.name());
 		
-		contextController.openRequest(r);
-		
-		r.setAttribute(nameParam, cotrix.name());
-		r.setAttribute(pwdParam, cotrix.name());
-		
-		User logged = service.login(r);
+		User logged = service.login(req);
 		
 		assertEquals(logged.name(),cotrix.name());
 		
@@ -85,34 +63,18 @@ public class LoginTest extends ApplicationTest {
 		
 		User user = user().name("fabio").email("fabio@me.com").fullName("fifi").build();
 		
-		signupService.signup(user,"fuffa");
+		signupService.signup(user,"any");
 		
 		assertTrue(user.can(EDIT.on(user.id())));
 		assertEquals(user.email(),"fabio@me.com");
-		
-		DummyHttpRequest r = new DummyHttpRequest();
-		
-		contextController.openRequest(r);
-		
-		r.setAttribute(nameParam, user.name());
-		r.setAttribute(pwdParam, "fuffa");
-		
-		User logged = service.login(r);
+
+		when(req.getAttribute(nameParam)).thenReturn(user.name());
+		when(req.getAttribute(pwdParam)).thenReturn("any");
+
+		User logged = service.login(req);
 		
 		assertEquals(logged.name(),user.name());
 		
-	}
-	
-	@Produces @SessionScoped
-	public @Current BeanSession currentUser() {
-	
-		return new BeanSession();		
-	}
-	
-	@Produces @RequestScoped
-	public @Current User currentUser(@Current BeanSession session) {
-	
-		return session.get(User.class);		
 	}
 
 }
