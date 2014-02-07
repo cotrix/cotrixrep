b3768b62a890cc499e4d2ff10e4b37d8187d78d0
Federico De Faveri <federico.defaveri@fao.org>
2014-02-07 10:00:50 +0100

    Added specific error messages for particular RPC exception (server unavailable, old application in browser) Updated AlertDialog style and centered when details are open

--- cotrix/cotrix-web-share/src/main/java/org/cotrix/web/share/client/error/ErrorManager.java
+++ cotrix/cotrix-web-share/src/main/java/org/cotrix/web/share/client/error/ErrorManager.java
@@ -5,7 +5,10 @@ package org.cotrix.web.share.client.error;
 
 import org.cotrix.web.share.client.util.Exceptions;
 import org.cotrix.web.share.client.widgets.AlertDialog;
+import org.cotrix.web.share.shared.exception.ServiceException;
 
+import com.google.gwt.user.client.rpc.IncompatibleRemoteServiceException;
+import com.google.gwt.user.client.rpc.InvocationException;
 import com.google.inject.Singleton;
 
 /**
@@ -17,9 +20,15 @@ public class ErrorManager {
 
 	public void rpcFailure(Throwable throwable) {
 		String details = Exceptions.getPrintStackTrace(throwable);
-		AlertDialog.INSTANCE.center("Ooops an error occurred on server side", details);
+		String errorMessage = getErrorMessage(throwable);
+		AlertDialog.INSTANCE.center(errorMessage, details);
 	}
-
-
-
+	
+	private String getErrorMessage(Throwable throwable) {
+		if (throwable instanceof IncompatibleRemoteServiceException) return "Looks like you're running an old version of Cotrix application, please <a href=\"http://en.wikipedia.org/wiki/Wikipedia:Bypass_your_cache\" target=\"_blank\">hard refresh</a> your browser";
+		if (throwable instanceof InvocationException) return "We are having some throubles trying to reach our servers, check if your connection is ok or retry in a few seconds";
+		if (throwable instanceof ServiceException) return "Ooops an error occurred on server side";
+		
+		return "Ooops an error occurred on server side";
+	}
 }
