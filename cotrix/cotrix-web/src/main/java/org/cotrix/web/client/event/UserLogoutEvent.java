a505b799137c496c2af2623876622210a56c1bdd
fabiosimeoni <fabio.simeoni@me.com>
2014-02-17 15:09:35 +0100

    pom refactorings against new web parent and new head modules

--- cotrix/cotrix-web/src/main/java/org/cotrix/web/client/event/UserLogoutEvent.java
+++ cotrix/cotrix-web/src/main/java/org/cotrix/web/client/event/UserLogoutEvent.java
@@ -1,7 +1,7 @@
 package org.cotrix.web.client.event;
 
-import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.event.shared.HasHandlers;
 
 /**
