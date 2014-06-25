bf3a98759271556c107d9c1317c76c690f717fc0
Federico De Faveri <federico.defaveri@fao.org>
2014-06-25 15:58:33 +0200

    Optimized attribute definition creation

--- cotrix/cotrix-neo/src/main/java/org/cotrix/neo/domain/NeoCodelist.java
+++ cotrix/cotrix-neo/src/main/java/org/cotrix/neo/domain/NeoCodelist.java
@@ -2,6 +2,10 @@ package org.cotrix.neo.domain;
 
 import static org.cotrix.neo.domain.Constants.NodeType.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.cotrix.domain.attributes.Attribute;
 import org.cotrix.domain.attributes.Definition;
 import org.cotrix.domain.codelist.Code;
 import org.cotrix.domain.codelist.Codelist;
@@ -34,13 +38,26 @@ public class NeoCodelist extends NeoVersioned implements Codelist.State {
 	
 	public NeoCodelist(Codelist.State state) {
 
-		super(CODELIST,state);	
+		super(CODELIST,state);
 		
-		for (Definition.State l : state.attributeTypes())
-			node().createRelationshipTo(NeoDefinition.factory.nodeFrom(l),Relations.DEFINITION);
+		Map<String, Definition.State> definitions = new HashMap<>();
+		
+		for (Definition.State l : state.attributeTypes()) {
+			Node definitionNode = NeoDefinition.factory.nodeFrom(l);
+			node().createRelationshipTo(definitionNode,Relations.DEFINITION);
+			definitions.put(l.id(), NeoDefinition.factory.beanFrom(definitionNode));
+		}
 		
 		for (CodelistLink.State l : state.links())
 			node().createRelationshipTo(NeoCodelistLink.factory.nodeFrom(l),Relations.LINK);
+		
+		//update attribute definitions
+		for (Code.State c : state.codes()) {
+			for (Attribute.State a: c.attributes()) {
+				Definition.State persistedState = definitions.get(a.definition().id());
+				if (persistedState!=null) a.definition(persistedState);
+			}
+		}
 		
 		for (Code.State c : state.codes())
 			node().createRelationshipTo(NeoCode.factory.nodeFrom(c),Relations.CODE);
