0a53073b5afaa14112ef6e1e1d4dd668e7d4b7ba
Federico De Faveri <federico.defaveri@fao.org>
2014-07-24 16:09:21 +0200

    Added id to logbook entry Fixed entry creation Made update method public in logbookservice Fixed logbook related permissions

--- cotrix/cotrix-application/src/main/java/org/cotrix/application/logbook/LogbookService.java
+++ cotrix/cotrix-application/src/main/java/org/cotrix/application/logbook/LogbookService.java
@@ -6,14 +6,14 @@ public interface LogbookService {
 	
 	
 	Logbook logbookOf(String id);
+	
+	void update(Logbook book);
 
 	interface Private extends LogbookService {
 		
 		void create(Logbook book);
 		
 		void removeLogbookOf(String id);
-
-		void update(Logbook book);
 		
 	}
 
