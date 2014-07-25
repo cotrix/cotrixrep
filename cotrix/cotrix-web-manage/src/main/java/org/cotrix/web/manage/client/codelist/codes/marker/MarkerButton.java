874fa56110b98b3b0e8350312df27fbcf7c2f1fe
Federico De Faveri <federico.defaveri@fao.org>
2014-07-25 09:27:36 +0200

    Highlight icon with letters

--- cotrix/cotrix-web-manage/src/main/java/org/cotrix/web/manage/client/codelist/codes/marker/MarkerButton.java
+++ cotrix/cotrix-web-manage/src/main/java/org/cotrix/web/manage/client/codelist/codes/marker/MarkerButton.java
@@ -7,7 +7,9 @@ import static org.cotrix.web.manage.client.codelist.codes.marker.MarkersResource
 
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.dom.client.Style.Float;
+import com.google.gwt.dom.client.Style.TextAlign;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.dom.client.Style.VerticalAlign;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
@@ -16,6 +18,7 @@ import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FocusPanel;
+import com.google.gwt.user.client.ui.Label;
 
 /**
  * @author "Federico De Faveri federico.defaveri@fao.org"
@@ -39,11 +42,19 @@ public class MarkerButton extends Composite implements HasValueChangeHandlers<Bo
 			}
 		});
 		
+		Label label = new Label(markerType.getName().substring(0, 1));
+		label.getElement().getStyle().setPaddingTop(2, Unit.PX);
+		
+		focusPanel.add(label);
+		
 		initWidget(focusPanel);
 		
 		down = false;
 		getElement().getStyle().setProperty("borderRadius", "2px");
 		getElement().getStyle().setFloat(Float.LEFT);
+		getElement().getStyle().setTextAlign(TextAlign.CENTER);
+		getElement().getStyle().setVerticalAlign(VerticalAlign.MIDDLE);
+		getElement().getStyle().setFontSize(12, Unit.PX);
 		setTitle(markerType.getName());
 		style();
 	}
