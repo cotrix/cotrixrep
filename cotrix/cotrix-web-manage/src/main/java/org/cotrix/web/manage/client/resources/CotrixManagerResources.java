8fee4572ee6279a31237d4645b24cd0954dd48f8
Federico De Faveri <federico.defaveri@fao.org>
2014-07-28 17:35:10 +0200

    Styled filter bar

--- cotrix/cotrix-web-manage/src/main/java/org/cotrix/web/manage/client/resources/CotrixManagerResources.java
+++ cotrix/cotrix-web-manage/src/main/java/org/cotrix/web/manage/client/resources/CotrixManagerResources.java
@@ -103,7 +103,6 @@ public interface CotrixManagerResources extends ClientBundle {
 	public ImageResource stopBullet();
 	
 	public ImageResource showMenu();
-	public ImageResource showMenuHover();
 	public ImageResource radioBullet();
 	public ImageResource checkBullet();
 	public ImageResource codesBullet();
