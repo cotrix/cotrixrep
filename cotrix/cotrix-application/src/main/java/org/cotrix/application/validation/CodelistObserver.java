7c645a8674af98f0bb382e4687202bdf81f09fb0
fabiosimeoni <fabio.simeoni@me.com>
2014-07-30 20:31:02 +0200

    codelists are validated in bulk when added to repos

--- cotrix/cotrix-application/src/main/java/org/cotrix/application/validation/CodelistObserver.java
+++ cotrix/cotrix-application/src/main/java/org/cotrix/application/validation/CodelistObserver.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import org.cotrix.common.events.Modified;
+import org.cotrix.common.events.New;
 import org.cotrix.domain.codelist.Codelist;
 import org.cotrix.repository.impl.EventProducer.UpdateActionEvent;
 
@@ -14,6 +15,14 @@ public class CodelistObserver {
 	@Inject
 	ValidationManager validator;
 
+
+	
+	public void onCodelistAddition(@Observes @New Codelist list){
+		
+		validator.check(list.id());
+		
+	}
+	
 	public void onCodelistUpdate(@Observes @Modified Codelist changeset){
 		
 		validator.check(changeset);
