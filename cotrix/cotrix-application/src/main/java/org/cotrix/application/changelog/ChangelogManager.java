0e775028278e9e3814927bb364f80373eca69378
fabiosimeoni <fabio.simeoni@me.com>
2014-07-30 09:01:18 +0200

    changelog computed in bulk when definitions are modified

--- cotrix/cotrix-application/src/main/java/org/cotrix/application/changelog/ChangelogManager.java
+++ cotrix/cotrix-application/src/main/java/org/cotrix/application/changelog/ChangelogManager.java
@@ -16,8 +16,10 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import org.cotrix.domain.attributes.Attribute;
+import org.cotrix.domain.attributes.AttributeDefinition;
 import org.cotrix.domain.codelist.Code;
 import org.cotrix.domain.codelist.Codelist;
+import org.cotrix.domain.codelist.LinkDefinition;
 import org.cotrix.domain.common.NamedStateContainer;
 import org.cotrix.domain.managed.ManagedCode;
 import org.cotrix.domain.trait.Status;
@@ -29,124 +31,163 @@ import com.google.gson.reflect.TypeToken;
 
 @Singleton
 public class ChangelogManager {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(ChangelogManager.class);
+
+	private static Type logtype = new TypeToken<List<ChangelogEntry>>() {
+	}.getType();
 
 	@Inject
 	private CodelistRepository codelists;
-	
-	
+
 	public void updateWith(Codelist changeset) {
 	
 		//we know this will succeed, an update has already taken place 
-		Codelist changed = codelists.lookup(changeset.id());
+		Codelist list = codelists.lookup(changeset.id());
+		
 		
 		//changelog requires lineage
-		if (manage(changed).hasno(PREVIOUS_VERSION)) 		
+		if (manage(list).hasno(PREVIOUS_VERSION)) 		
 			return;
-				
-		for (Code change : changeset.codes()) {
 		
+		Codelist.Private plist = reveal(list);
+		Codelist.Private pchangeset = reveal(changeset);
+		
+		if (isBulkUpdate(pchangeset))
+		
+			processBulk(plist);
+		
+		else //exclusive because bulk-handling currently processes all codes
+			
+			processPunctual(plist, pchangeset);
+	}
+	
+	private void processBulk(Codelist.Private list) {
+		
+		//this seems coarse: any bulk change triggers a full traversal
+		//in practice we do expect shared defs to occur in most-to-all codes.
+		//even if the impacted codes were a few, how much overhead would we be adding to the 
+		//traversal required underneatah to find the impact right subset of codes? 
+		//would need to measure. surely, fine-grain requires more work and we should not
+				
+		for (Code.Private changed : list.codes()) 
+			handleModifiedMarkerWith(changed);
+	}
+
+	private void processPunctual(Codelist.Private list, Codelist.Private changeset) {
+
+		for (Code.Private change : changeset.codes()) {
+
 			Status status = reveal(change).status();
-			
-			//changelog is pointless if the code is to be removed anyway
-			if (status==Status.DELETED)
+
+			// changelog is pointless if the code is to be removed anyway
+			if (status == Status.DELETED)
 				continue;
-			
-			Code code = changed.codes().lookup(change.id());
-			
-			processCode(reveal(code),reveal(change));
+
+			Code.Private code = list.codes().lookup(change.id());
+
+			processCode(code,change);
 		}
 	}
-	
-	
+
 	private void processCode(Code.Private changed, Code.Private change) {
-		
-		NamedStateContainer<Attribute.State>  attributes = changed.state().attributes();
-		
-		if (change.status()==null)
-			
-			aaddNewMarkerTo(attributes);
-		
-		else 
-			
-			handleModifiedMarkerWith(changed, change, attributes);
-		
-		
+
+		if (change.status() == null)
+
+			aaddNewMarkerTo(changed.state().attributes());
+
+		else
+
+			handleModifiedMarkerWith(changed);
+
 	}
-	
-	private void aaddNewMarkerTo(NamedStateContainer<Attribute.State>  attributes) {
-		
+
+	private void aaddNewMarkerTo(NamedStateContainer<Attribute.State> attributes) {
+
 		attributes.add(stateof(attribute().instanceOf(NEW).value("TRUE")));
 	}
-	
-	
-	private void handleModifiedMarkerWith(Code.Private changed, Code.Private change, NamedStateContainer<Attribute.State>  attributes) {
-		
+
+	private void handleModifiedMarkerWith(Code.Private changed) {
+
+		NamedStateContainer<Attribute.State> attributes = changed.state().attributes();
+
 		ManagedCode managed = manage(changed);
+
 		Attribute modified = managed.attribute(MODIFIED);
 
 		String originId = managed.originId();
-		
-		//by now we know codelist has lineage, but this update may be for a new code
-		if (hasno(originId))
+
+		// by now we know codelist has lineage, but this update may be for a new
+		// code
+		if (notmarked(originId))
 			return;
-		
-		//fetch the past
+
+		// fetch the past
 		Code origin = codelists.get(code(originId));
-		
-		if (hasno(origin)) {
-			log.error("cannot compute changelog for code {} as its lineage {} can't be retrieved.",change.id(),originId);
+
+		if (notmarked(origin)) {
+			log.error("cannot compute changelog for code {} as its lineage {} can't be retrieved.", changed.id(), originId);
 			return;
 		}
-		
+
 		ChangelogProducer producer = new ChangelogProducer();
-		
+
 		Changelog log = producer.changesBetween(origin, changed);
-		
-		if (log.entries().isEmpty()) {
-			
-			if (has(modified))
-				
+
+		if (log.isEmpty()) {
+
+			if (marked(modified))
+
 				attributes.remove(modified.id());
 
 			return;
-			
+
 		}
-		
-		Type entrytype = new TypeToken<List<ChangelogEntry>>(){}.getType();
-		
-		if (hasno(modified)) {
-			
+
+		if (marked(modified)) {
+
+			String oldlog = modified.value();
+
+			List<ChangelogEntry> oldentries = jsonBinder().fromJson(oldlog, logtype);
+
+			log.addAll(oldentries);
+
+		} else {
+
 			attributes.add(stateof(attribute().instanceOf(MODIFIED)));
-			modified = managed.attribute(MODIFIED);
+			modified = managed.attribute(MODIFIED); // remember persistence
+
 		}
-		else {
-			
-			List<ChangelogEntry> existingEntries = jsonBinder().fromJson(modified.value(),entrytype) ;
-			
-			log.addAll(existingEntries);
-			
-			
-		}
-		
 
 		List<ChangelogEntry> entries = log.entries();
 
 		sort(entries);
-			
-		stateof(modified).value(jsonBinder().toJson(entries,entrytype));
-		
-	}
-	
 
-	
-	//helper
-	private boolean has(Object o) {
-		return o!=null;
+		stateof(modified).value(jsonBinder().toJson(entries, logtype));
+
 	}
-	
-	private boolean hasno(Object o) {
-		return o==null;
+
+	// helper
+	private boolean isBulkUpdate(Codelist.Private changeset) {
+
+		// at least one change to definitions, other than additions
+
+		for (AttributeDefinition.Private def : changeset.definitions())
+			if (def.status() != null)
+				return true;
+
+		for (LinkDefinition.Private def : changeset.links())
+			if (def.status() != null)
+				return true;
+
+		return false;
+	}
+
+	private boolean marked(Object o) {
+		return o != null;
+	}
+
+	private boolean notmarked(Object o) {
+		return o == null;
 	}
 }
