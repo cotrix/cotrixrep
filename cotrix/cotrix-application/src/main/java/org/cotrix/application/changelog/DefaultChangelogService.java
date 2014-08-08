9528efb9d9c4bd0b6005b1083017f39da71b8fbd
fabiosimeoni <fabio.simeoni@me.com>
2014-08-08 16:30:30 +0200

    changelog design slightly simpler

--- cotrix/cotrix-application/src/main/java/org/cotrix/application/changelog/DefaultChangelogService.java
+++ cotrix/cotrix-application/src/main/java/org/cotrix/application/changelog/DefaultChangelogService.java
@@ -18,6 +18,7 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import org.cotrix.application.ChangelogService;
 import org.cotrix.common.async.CancelledTaskException;
 import org.cotrix.common.async.TaskContext;
 import org.cotrix.common.async.TaskUpdate;
@@ -27,7 +28,6 @@ import org.cotrix.domain.codelist.Codelist;
 import org.cotrix.domain.common.NamedContainer;
 import org.cotrix.domain.common.NamedStateContainer;
 import org.cotrix.domain.managed.ManagedCode;
-import org.cotrix.domain.managed.ManagedCodelist;
 import org.cotrix.domain.trait.Status;
 import org.cotrix.repository.CodelistRepository;
 import org.slf4j.Logger;
@@ -36,16 +36,17 @@ import org.slf4j.LoggerFactory;
 import com.google.gson.reflect.TypeToken;
 
 @Singleton
-public class ChangelogManager {
+public class DefaultChangelogService implements ChangelogService {
 
-	private static final Logger log = LoggerFactory.getLogger(ChangelogManager.class);
+	
+	private static final Logger log = LoggerFactory.getLogger(DefaultChangelogService.class);
 
-	private static Type logtype = new TypeToken<List<ChangelogEntry>>() {
-	}.getType();
+	private static Type logtype = new TypeToken<List<ChangelogEntry>>() {}.getType();
 
 	@Inject
 	private CodelistRepository codelists;
 
+	
 	public void trackAfter(Codelist changeset) {
 	
 		//we know this will succeed, an update has already taken place 
@@ -61,61 +62,9 @@ public class ChangelogManager {
 		trackPunctual(plist, pchangeset);
 	}
 	
-	public void track(Codelist list, boolean optimise) {
-		
-		Codelist.Private plist = reveal(list);
-		
-		if (optimise)
-			trackBulkOptimised(plist);
-		else
-			trackBulk(reveal(plist));
-	}
 	
-	private void trackBulk(Codelist.Private list) {
-		
-		//this seems coarse: any schema change triggers a full traversal
-		//in practice we do expect changes to propagate to most-to-all codes.
-		//mostly we cannot conveniently detect that codes have changed because the schema has.
-		
-		TaskContext context = new TaskContext();
-		float progress=0f;
-		
-		NamedContainer<? extends Code.Private> codes =  reveal(list).codes();
-		
-		int total = list.codes().size();
-		
-		//arbitrary
-		long step = round(max(10,floor(codes.size()/10)));
-		
-		int i=0;
-		
-		long time = currentTimeMillis();
-		
-		for (Code.Private code : codes) {
-		
-			i++;
-			progress++;
-			
-			handleModifiedMarkerWith(code);
-			
-			if (i%step==0)
-				
-				if (context.isCancelled()) {
-					log.info("cahngelog tracking aborted on user request after {} codes.",i);
-					throw new CancelledTaskException("changelog tracking aborted on user request");
-				}
-			
-				else {
-				
-					context.save(TaskUpdate.update(progress/total, "tracked "+i+" codes"));
-				}
-		}
-		
-
-		log.trace("tracked changelog for {} in {} msec.",signatureOf(list),currentTimeMillis()-time);
-	}
-	
-	private void trackBulkOptimised(Codelist.Private list) {
+	@Override
+	public void track(Codelist list, boolean optimised) {
 		
 		TaskContext context = new TaskContext();
 
@@ -132,23 +81,22 @@ public class ChangelogManager {
 		
 		long time = currentTimeMillis();
 		
+		Date listCreated = manage(list).created();
 		
-		ManagedCodelist mlist = ManagedCodelist.manage(list);
-		
-		Date listCreated = mlist.created();
+		ManagedCode mcode = new ManagedCode();
 		
 		for (Code.Private code : codes) {
 		
 			i++;
 			progress++;
 			
-			ManagedCode mcode = manage(code);
-
+			mcode.set(code);
+			
 			Date codeUpdated = mcode.lastUpdated();
 			
-			//process only if it has changed since the list was created
+			//process only if it has changed since this list's version was created
 			
-			if (codeUpdated!=null && mcode.lastUpdated().after(listCreated))
+			if (!optimised || (codeUpdated!=null && optimised && codeUpdated.after(listCreated)))
 			
 				handleModifiedMarkerWith(code);
 			
@@ -160,10 +108,10 @@ public class ChangelogManager {
 					throw new CancelledTaskException("changelog tracking aborted on user request");
 				}
 			
-				else {
+				else
 				
 					context.save(TaskUpdate.update(progress/total, "tracked "+i+" codes"));
-				}
+				
 		}
 		
 
@@ -174,10 +122,8 @@ public class ChangelogManager {
 
 		for (Code.Private change : changeset.codes()) {
 
-			Status status = reveal(change).status();
-
 			// changelog is pointless if the code is to be removed anyway
-			if (status == Status.DELETED)
+			if (change.status() == Status.DELETED)
 				continue;
 
 			Code.Private code = list.codes().lookup(change.id());
